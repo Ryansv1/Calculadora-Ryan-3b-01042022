a4a1e0dd80f0a6f1a26e0e1cd7c47ce30bd0e5de
Ryansv1 <ryaan.gs2020@gmail.com>
2022-04-01 09:02:24 -0300

    sla

---
+++ calculadora.js.js
@@ -0,0 +1,9 @@
+var dividir = require('./modules/dividir')
+var multiplicar = require('./modules/multiplicar')
+var subtrair = require('./modules/subtrair')
+var somar = require('./modules/somar')
+
+console.log(somar(15,67));
+console.log(subtrair(15,67));
+console.log(dividir(15,67));
+console.log(multiplicar(15,67));
